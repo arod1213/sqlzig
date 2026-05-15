580564ecb1be5e4af9c26535f0b3730c991d2ccd
Aidan Rodriguez <aidan@aidanrodriguez.com>
2026-05-15 12:28:22 -0700

    forgot to put blk back

--- src/stmt.zig
+++ src/stmt.zig
@@ -157,7 +157,7 @@ fn bindValue(stmt: ?*c.sqlite3_stmt, idx: c_int, param: anytype) !void {
             const enum_int = @intFromEnum(param);
             break :blk c.sqlite3_bind_int(stmt, idx, @intCast(enum_int));
         },
-        .optional => {
+        .optional => blk: {
             if (param == null) {
                 break :blk c.sqlite3_bind_null(stmt, idx);
             } else {
